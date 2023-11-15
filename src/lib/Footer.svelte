e69afa52858a0359646e361956923364136888f0
raquel-1 <rocamartinezraquel@gmail.com>
2023-11-15 13:05:28 +0100

    boton ver todas las peliculas desaparece cuando lo clickeo

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -5,14 +5,17 @@
   function handleClick(e) {
     mostrar=true;
   }
+
+
 </script>
 
 <footer class={$darkmode ? "darkmode" : ""}>
-  <!--<Movies/>-->
-  <button on:click={handleClick}>Todas las peliculas</button>
+  
   {#if mostrar}
-<Movies/>
-  {/if}
+    <Movies/>
+    {:else}
+      <button on:click={handleClick} class="btn btn-category" >Todas las peliculas</button>
+    {/if}
 </footer>
 
 <style>
@@ -24,4 +27,29 @@
     padding-left: 5rem;
     padding-right: 5rem;
   }
+  .btn-category {
+  width: 20rem;
+  height: 50%;
+  background-image: none;
+}
+
+.btn {
+  margin: 1rem;
+  border: none;
+  font-family: 'Coiny', sans-serif;
+  line-height: 1;
+  text-transform: uppercase;
+  font-size: 1rem;
+  padding: 16px 0 13px;
+  color: #fafaf9;
+  border-radius: 100px;
+  cursor: pointer;
+  transition: 0.3s;
+  background-image: linear-gradient(135deg, grey, var(--blue));
+
+}
+.btn:hover {
+    transform: scale(110%) rotate(-2deg);
+}
+
 </style>
