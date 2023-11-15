aea34e8838b5085debb8c69977b1a7da1eb99a14
iredica <iredica@gmail.com>
2023-11-15 12:54:00 +0100

    Activado el boton Todas las peliculas

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,21 +1,27 @@
 <script>
-
   import Movies from "./Movies.svelte";
   import { darkmode } from "$stores/store";
-  
+  var mostrar=false
+  function handleClick(e) {
+    mostrar=true;
+  }
 </script>
-  
-  <footer class={$darkmode?'darkmode':''}>
-  <Movies />
-  </footer>
-  
+
+<footer class={$darkmode ? "darkmode" : ""}>
+  <!--<Movies/>-->
+  <button on:click={handleClick}>Todas las peliculas</button>
+  {#if mostrar}
+<Movies/>
+  {/if}
+</footer>
+
 <style>
-  :global(footer.darkmode){
+  :global(footer.darkmode) {
     background-color: black;
     color: white;
   }
-    footer{
-      padding-left: 5rem;
-      padding-right: 5rem;
-    }
+  footer {
+    padding-left: 5rem;
+    padding-right: 5rem;
+  }
 </style>
--- src/lib/Movies.svelte
+++ src/lib/Movies.svelte
@@ -5,52 +5,15 @@
 
   export let movies = samples.peliculas || [];
 
-  let estrenos = movies.slice(6, 24);
+  //let estrenos = movies.slice(6, 24);
   let count=movies.length;
 </script>
 
 
   <h1>Peliculas (numero total de peliculas {count})</h1>
-  <Cartas   cartas={estrenos}  />
+  <Cartas   cartas={movies}  />
     
   
-
-<!-- 
-  <h1>Peliculas (numero total de peliculas {count})</h1>
-  <div class="cartas">
-    {#each estrenos as movie}
-      <div class="carta">
-        <div class="caja">
-          <div class="imagen">
-            <img class="imagen-pelicula" src={movie.thumbnail} alt="" />
-          </div>
-
-          <div class="contenido">
-            <a id="nombre">{movie.title}</a>
-          </div>
-        </div>
-      </div>
-    {/each}
-  </div>
-</div>
-
-
-        <img class="imagen-pelicula" src={movie.photo} alt="">
-        <p class="titulo">{movie.name}</p>
-        <ul>
-        {#each movie.genre as genero}
-            <li>||{genero}||</li>
-        {/each}
-        </ul>
-
-         <ul>
-        {#each movie.actors as actor}
-            <li>
-                <p>{actor.name}</p>
-                <img class="imagen-actor" src={actor.photo} alt="" >
-            </li>
-        {/each}
-        </ul> -->
 
 <style>
 
